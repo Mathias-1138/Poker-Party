5efdc6f3fffa5e98a8dfa0d123bf90479422bba8
mathias-1138 <mathiasgut08@gmail.com>
2025-02-24 19:06:18 -0800

    True final product with comments

--- src/players/mathiasPlayer.java
+++ src/players/mathiasPlayer.java
@@ -51,19 +51,19 @@ public class mathiasPlayer extends Player {
         return evaluatePlayerHand().getValue() < HandRanks.PAIR.getValue()
                 && getGameState().isActiveBet();
     }
-
+// if the hand is bad and far into the game, the player folds.
     @Override
     protected boolean shouldCheck() {
        return !getGameState().isActiveBet();
  }
-
+ // if the hand is not that bad, it will check to see how their hand goes.
     @Override
     protected boolean shouldCall() {
         handRank = evaluatePlayerHand();
         return handRank.getValue() >= HandRanks.PAIR.getValue() &&
                 handRank.getValue() < HandRanks.THREE_OF_A_KIND.getValue();
     }
-
+// calls if the hand is good but not enough to justify making big plays
     @Override
     protected boolean shouldRaise() {
         HandRanks handRank = evaluatePlayerHand();
@@ -72,10 +72,11 @@ public class mathiasPlayer extends Player {
         return handRank.getValue() >= HandRanks.THREE_OF_A_KIND.getValue()
                 && bank > currentBet * 3;
     }
-
+// Raises when the hand is good and raises aggressively because it is confident that its hand is better
     @Override
     protected boolean shouldAllIn() {
         handRank = evaluatePlayerHand();
         return handRank.getValue() >= HandRanks.FOUR_OF_A_KIND.getValue();
     }
 }
+// goes all in when the hand is amazing it goes in hard and towards the later parts of the game
